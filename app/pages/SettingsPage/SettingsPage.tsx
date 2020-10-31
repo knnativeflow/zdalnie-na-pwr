6d1c64874157dc16a3a00ab2f0a56f58cc55c4b1
Karol Sitarz <ka.sitarz@gmail.com>
2020-10-31 19:34:02 +0100

    add temporary logout button for quick debugging change pink.light color

--- app/pages/SettingsPage/SettingsPage.tsx
+++ app/pages/SettingsPage/SettingsPage.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import BottomAppBar from 'components/BottomAppBar'
+import Button from 'components/Button'
+import { APP_COLORS } from 'base/theme/theme'
+import { useDispatch } from 'react-redux'
+import { clearUser } from 'actions/user'
 
 const SettingsPage = () => {
+  const dispatch = useDispatch()
+  const logoutUser = () => dispatch(clearUser())
   return (
     <div>
+      <Button color={APP_COLORS.blue.main} primary shadow onClick={logoutUser}>
+        Wyloguj
+      </Button>
       <BottomAppBar />
     </div>
   )
--- app/reducers/events.ts
+++ app/reducers/events.ts
@@ -1,5 +1,6 @@
 import { ADD_EVENT_ZOOM_LINKS, ADD_EVENTS, CLEAR_EVENTS } from 'constants/actionTypes'
 import { ActionEvents } from 'actions/events'
+import { IEvent } from 'domain/event'
 
 const INIT_STATE: IEvent[] = []
 
@@ -8,7 +9,7 @@ const eventsReducer = (state = INIT_STATE, action: ActionEvents) => {
     case ADD_EVENTS:
       return action.payload ?? state
     case CLEAR_EVENTS:
-      return []
+      return INIT_STATE
     case ADD_EVENT_ZOOM_LINKS:
       return state.map((event) => {
         const foundLink = action.payload.find(
