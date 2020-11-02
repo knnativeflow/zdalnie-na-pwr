7386a3af2a2585733b76022f8e691a46ca5467b6
Mitelak <mitelak@gmail.com>
2020-11-02 22:16:42 +0100

    Fix settings page temporary

--- app/pages/SettingsPage/SettingsPage.tsx
+++ app/pages/SettingsPage/SettingsPage.tsx
@@ -1,9 +1,7 @@
 import React, { SyntheticEvent } from 'react'
 import { useDispatch } from 'react-redux'
 import { shell } from 'electron'
-import Button from 'components/Button'
 
-import { THEME } from 'base/theme/theme'
 import { clearUser } from 'actions/user'
 import styled from '@emotion/styled'
 
@@ -11,11 +9,33 @@ const APP_VERSION = process.env.npm_package_version
 
 const Wrapper = styled.div`
   padding: 16px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+// TODO: use correct colors
+const Button = styled.button`
+  border: none;
+  background: #5c7ca8;
+  color: #fff;
+  border-radius: 16px;
+  padding: 16px 24px;
+  font-weight: 700;
+  transition: all 0.1s ease-in-out;
+  box-shadow: 0 5px 8px -2px #cbd9ec;
+  cursor: pointer;
+  margin-bottom: 24px;
+
+  &:hover {
+    background: #5378ac;
+  }
 `
 
 const Text = styled.p`
   font-size: 15px;
-  color: #444;
+  color: #292b31;
 `
 
 const SettingsPage = () => {
@@ -29,22 +49,20 @@ const SettingsPage = () => {
 
   return (
     <Wrapper>
-      <Button color={THEME.colors.palette.blue.main} variant="primary" shadow onClick={logoutUser}>
-        Wyloguj
-      </Button>
+      <Button onClick={logoutUser}>Wyloguj</Button>
 
       <Text>
         <a href="https://zdalnie.napwr.pl" onClick={handleLink}>
           O aplikacji
         </a>
       </Text>
-      <Text>Wersja aplikacji {APP_VERSION}</Text>
       <Text>
         Rozwijane przez{' '}
         <a href="https://knnativeflow.napwr.pl" onClick={handleLink}>
           KN Native Flow
         </a>
       </Text>
+      <Text>Wersja aplikacji {APP_VERSION}</Text>
     </Wrapper>
   )
 }
