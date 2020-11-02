918283eb8c70ecb020fc85eadbe346100ef41f44
Mitelak <mitelak@gmail.com>
2020-11-02 22:00:06 +0100

    Extract dashboard styled components to external file

--- app/pages/DashboardPage/DashboardPage.tsx
+++ app/pages/DashboardPage/DashboardPage.tsx
@@ -1,89 +1,24 @@
-import styled from '@emotion/styled'
-import EventInfo from 'components/EventInfo'
-import { IEvent } from 'domain/event'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { RootState } from 'store'
-import { eventColor } from 'utils/courseTypes'
 import moment from 'moment'
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100%;
-`
-
-const Title = styled.p`
-  font-weight: 700;
-  margin: 0;
-  margin-bottom: 24px;
-`
-
-const ComingLessonsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 16px;
-  flex: 1;
-  overflow: auto;
-`
-
-const EventInfoWrapper = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  background: #f5f9fd;
-  overflow: auto;
-`
-
-const EventsList = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`
-
-const EventItemWrapper = styled.li`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-`
-
-const EventItemButton = styled.button`
-  width: 100%;
-  background: none;
-  border: none;
-  display: flex;
-  background: #f3f5f8;
-  border-radius: 16px;
-  padding: 20px;
-`
-
-const EventItemColor = styled.div<{ color: string }>`
-  width: 8px;
-  height: 45px;
-  background: ${({ color }) => color};
-  margin-right: 16px;
-  border-radius: 5px;
-`
-
-const EventInfoContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`
-
-const EventItemTime = styled.p`
-  margin: 0;
-  font-weight: 600;
-  font-size: 14px;
-  color: #405775;
-`
-
-const EventItemName = styled.p`
-  margin: 0;
-  font-weight: 900;
-  font-size: 18px;
-  color: #405775;
-`
+import EventInfo from 'components/EventInfo'
+import { IEvent } from 'domain/event'
+import { RootState } from 'store'
+import { eventColor } from 'utils/courseTypes'
+import {
+  ComingLessonsWrapper,
+  EventInfoContent,
+  EventInfoWrapper,
+  EventItemButton,
+  EventItemColor,
+  EventItemName,
+  EventItemTime,
+  EventItemWrapper,
+  EventsList,
+  Wrapper,
+  Title,
+} from './DashboardPage.styled'
 
 type EventItemProps = { event: IEvent; onClick: () => void }
 
@@ -102,14 +37,14 @@ const EventItem = ({ event, onClick }: EventItemProps) => {
   )
 }
 
-const isEndTimeMoreThanPresent = (event: IEvent) => new Date(event.end).getTime() > new Date().getTime()
+const isEndTimeAfterPresent = (event: IEvent) => new Date(event.end).getTime() > new Date().getTime()
 const sortByEndTime = (a: IEvent, b: IEvent) => (new Date(a.end).getTime() > new Date(b.end).getTime() ? 1 : -1)
 
 const DashboardPage = () => {
   const [choosenEvent, setChoosenEvent] = useState<IEvent>()
   const events = useSelector((state: RootState) => state.events)
 
-  const comingEvents = events.filter(isEndTimeMoreThanPresent).sort(sortByEndTime).slice(0, 5)
+  const comingEvents = events.filter(isEndTimeAfterPresent).sort(sortByEndTime).slice(0, 5)
 
   return (
     <Wrapper>
