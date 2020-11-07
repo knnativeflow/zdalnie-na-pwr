c1880664ec893eb70564f8d6f62b9ff93451bb0a
maciej.kopec <dimitor115@gmail.com>
2020-11-07 13:26:09 +0100

    Add DialogWrapper for styling smail password change modal

--- app/components/SmailModal/SmailPasswordChangeModal.tsx
+++ app/components/SmailModal/SmailPasswordChangeModal.tsx
@@ -7,19 +7,21 @@ import {
 
 import Button from 'components/Button'
 import studentMail from 'features/studentMail'
-import styles from './SmailPasswordChangeModal.scss'
 import { THEME } from 'base/theme/theme'
 import LoginForm from 'components/LoginForm'
 import PasswordManager from 'features/passwords'
 import { mailValidationSchema } from 'pages/ConfigurationPage/validationsSchemas'
 import { RootState } from 'store'
 import Space from '../Space'
+import styled from '@emotion/styled'
 
 interface Props {
   open: boolean
   onSuccess: () => void
   onClose: () => void
 }
+
+const DialogContent = styled.div`padding: 10px 30px 20px 30px`
 
 const SmailPasswordChangeModal = (props: Props): JSX.Element => {
   const { open, onSuccess, onClose } = props
@@ -36,17 +38,19 @@ const SmailPasswordChangeModal = (props: Props): JSX.Element => {
   }
 
   return (
-    <Dialog open={open} onClose={onClose} classes={{ root: styles.modal }}>
-      <DialogTitle>Zmień hasło do poczty studenckiej</DialogTitle>
-      <Space size={1} />
-      <LoginForm
-        color={THEME.colors.palette.purple}
-        {...{ onSubmit, defaultValues, validationSchema: mailValidationSchema, disabledFields }}
-        loginPlaceholder="pwr######"
-      />
-      <Button glow color={THEME.colors.palette.purple.dark} fullWidth variant="primary" onClick={onClose}>
-        Anuluj
-      </Button>
+    <Dialog open={open} onClose={onClose}>
+      <DialogContent>
+        <DialogTitle>Zmień hasło do poczty studenckiej</DialogTitle>
+        <Space size={1} />
+        <LoginForm
+          color={THEME.colors.palette.purple}
+          {...{ onSubmit, defaultValues, validationSchema: mailValidationSchema, disabledFields }}
+          loginPlaceholder="pwr######"
+        />
+        <Button glow color={THEME.colors.palette.purple.dark} fullWidth variant="primary" onClick={onClose}>
+          Anuluj
+        </Button>
+      </DialogContent>
     </Dialog>
   )
 }
