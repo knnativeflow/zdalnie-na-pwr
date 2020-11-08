620c09cf284cfc8c1080235ac994db1b7e2a8065
Mitelak <mitelak@gmail.com>
2020-11-08 18:26:26 +0100

    feat(sentry): add new options to sentry configuration

--- app/initSentry.ts
+++ app/initSentry.ts
@@ -1,9 +1,13 @@
 import * as Sentry from '@sentry/electron'
+import { CaptureConsole } from '@sentry/integrations'
 
 const initSentry = () => {
   if (process.env.NODE_ENV === 'production') {
     Sentry.init({
       dsn: process.env.SENTRY_DSN,
+      release: process.env.npm_package_version,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      integrations: [new CaptureConsole({ levels: ['error'] }) as any],
     })
   }
 }
