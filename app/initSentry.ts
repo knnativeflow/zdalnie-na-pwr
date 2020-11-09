65a776e6ac8123ceb336faa10b729b890452fc89
Marek Waszkowski <marek.waszkowski@gmail.com>
2020-11-09 21:19:30 +0100

    feat(sentry): get app version from electron data

--- app/initSentry.ts
+++ app/initSentry.ts
@@ -1,11 +1,12 @@
 import * as Sentry from '@sentry/electron'
 import { CaptureConsole } from '@sentry/integrations'
+import { remote } from 'electron'
 
 const initSentry = () => {
   if (process.env.NODE_ENV === 'production') {
     Sentry.init({
       dsn: process.env.SENTRY_DSN,
-      release: process.env.npm_package_version,
+      release: remote.app.getVersion(),
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       integrations: [new CaptureConsole({ levels: ['error'] }) as any],
     })
